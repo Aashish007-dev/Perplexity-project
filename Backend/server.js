e61dff532c0ec06220d83c16d8ccb43a573b6943
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-08-01 16:34:37 +0530

    Implement Socket.IO for real-time communication, add socket connection handling, and update routing

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import app from './src/app.js'
 import { connectToDB } from './src/config/database.js';
+import http from 'http';
+import { initSocketServer } from './src/sockets/server.socket.js';
 
+const httpServer = http.createServer(app);
+
+initSocketServer(httpServer);
 
 
 connectToDB();
 
 
-app.listen(3000, () => {
+httpServer.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
